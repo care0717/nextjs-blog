a4fbb1bf64de3a6bc41ff3a1d52fd6fb6274386e
care0717 <dev.care0717@gmail.com>
2020-10-19 23:45:10 +0900

    Initial commit from Create Next App

---
+++ src/pages/posts/first-post.tsx
@@ -0,0 +1,18 @@
+import {FC} from "react";
+import * as React from "react";
+import Link from 'next/link'
+
+const FirstPost: FC = () => {
+    return (
+        <>
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">
+                    <a>Back to home</a>
+                </Link>
+            </h2>
+        </>
+    )
+}
+
+export default FirstPost
