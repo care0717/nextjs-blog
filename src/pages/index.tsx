f1d9c3a3f87025218826f26cff49781af5293db2
care0717 <dev.care0717@gmail.com>
2020-10-20 20:25:47 +0900

    feat: add static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,42 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import {FC} from "react";
-import Link from "next/link";
+import { getSortedPostsData, PostsData } from '../lib/posts'
 
-const Home: FC = () => {
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData()
+    return {
+        props: {
+            allPostsData
+        }
+    }
+}
+
+interface Props {
+    allPostsData: PostsData[]
+}
+
+const Home: FC<Props> = ({ allPostsData }) => {
   return (
       <Layout home>
         <Head>
           <title>{siteTitle}</title>
         </Head>
-        <section className={utilStyles.headingMd}>
-          <p>I'm care0717. Read <Link href="/posts/first-post"><a>this page!</a></Link></p>
-          <p>
-            (This is a sample website - you’ll be building a site like this on{' '}
-            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-          </p>
-        </section>
+          <section className={utilStyles.headingMd}>…</section>
+          <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+              <h2 className={utilStyles.headingLg}>Blog</h2>
+              <ul className={utilStyles.list}>
+                  {allPostsData.map(({ id, date, title }) => (
+                      <li className={utilStyles.listItem} key={id}>
+                          {title}
+                          <br />
+                          {id}
+                          <br />
+                          {date}
+                      </li>
+                  ))}
+              </ul>
+          </section>
       </Layout>
   )
 }
